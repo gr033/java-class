9bb682491d07370d96d2890d725da7fb1fb3e1ed
gr033 <128104387+gr033@users.noreply.github.com>
2023-03-22 17:36:57 +0900

    Create SuperTest.java

---
+++ 2023/0322/SuperTest.java
@@ -0,0 +1,16 @@
+class Person{
+	public Person(String name){
+		
+	}
+}
+class Customer extends Person
+{
+}
+class SuperTest
+{
+	public static void main(String[] args) 
+	{
+		Customer c = new Customer();
+		System.out.println("OK");
+	}
+}
